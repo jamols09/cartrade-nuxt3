cac7d9d30b6c357709c9b35382654bd497effd46
jamols09 <jamols09@github.com>
2023-05-01 21:02:10 +0800

    feat: add pages and component

---
+++ components/Car/SearchBar.vue
@@ -0,0 +1,30 @@
+<script setup>
+const city = ref("");
+const router = useRouter();
+const error = ref(false);
+
+const onSearch = () => {
+  if (!city.value) {
+    return (error.value = true);
+  }
+  navigateTo(`/city/${city.value}/car`);
+};
+</script>
+<template>
+  <!-- HOME SEARCH BAR -->
+  <div
+    class="font-serif w-[1000px] text-2xl rounded-full bg-white flex justify-between overflow-hidden drop-shadow-2xl mx-auto"
+  >
+    <input
+      type="text"
+      class="py-3 px-5 w-full text-2xl rounded-full focus:outline-none"
+      :class="error ? 'border-red-500 border' : ''"
+      placeholder="Search by city..."
+      v-model="city"
+    />
+    <button class="bg-sky-500 px-10 text-white" @click="onSearch">
+      Search
+    </button>
+  </div>
+  <!-- HOME SEARCH BAR -->
+</template>
--- pages/city/[city]/car/[[make]].vue
+++ pages/city/[city]/car/[[make]].vue
@@ -1,5 +1,6 @@
 <template>
   <div>
-    <NuxtPage />
+    <CarCard />
   </div>
+  <!-- CARS PAGE -->
 </template>
