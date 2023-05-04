258266c084f9024627075f6f4e2a029efc44cb53
jamols09 <jamols09@github.com>
2023-05-04 22:00:01 +0800

    feat: add dev tools module

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,12 +1,13 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
-	modules: [
-		"@nuxtjs/tailwindcss",
-		"@nuxt/image-edge",
-		'@nuxt/devtools',
-	],
-	devtools: {
-		// Enable devtools (default: true)
-		enabled: true,
-	}
+    modules: [
+        "@nuxtjs/tailwindcss",
+        "@nuxt/image-edge",
+        '@nuxt/devtools',
+        '@vueuse/nuxt',
+    ],
+    devtools: {
+        // Enable devtools (default: true)
+        enabled: true,
+    }
 });
