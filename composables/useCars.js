0763577f5ce85e26e882a2abf3036131bb086592
jamols09 <jamols09@github.com>
2023-05-04 22:00:40 +0800

    refactor: change file path of data

--- composables/useCars.js
+++ composables/useCars.js
@@ -1,7 +1,9 @@
-import cars from "@/cars.json";
+import cars from "@/data/cars.json";
+import make from "@/data/make.json";
 
 export const useCars = () => {
   return {
     cars,
+    make,
   };
 };
