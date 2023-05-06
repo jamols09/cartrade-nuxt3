ac6fcce57d6a345af3db3698b8d12f8360d6a7e0
jamols09 <jamols09@github.com>
2023-05-06 17:15:38 +0800

    feat: add new pages for car listing

---
+++ pages/profile/listings/create.vue
@@ -0,0 +1,40 @@
+<script setup>
+definePageMeta({
+  layout: "custom",
+});
+
+const { makes } = useCars();
+const info = useState("adInfo", () => {
+  return {
+    make: "",
+    model: "",
+    year: "",
+    miles: "",
+    price: "",
+    city: "",
+    seats: "",
+    description: "",
+    image: null,
+  };
+});
+
+const onChangeInput = (data, name) => {
+  info.value[name] = data;
+};
+</script>
+
+<template>
+  <div>
+    <div class="mt-24">
+      <h1 class="text-6xl mt-24">Create a New Listing</h1>
+    </div>
+    <div class="shadow rounded p-3 mt-5 flex flex-wrap justify-between">
+      <CarAdSelect
+        title="Make *"
+        :options="makes"
+        name="make"
+        @change-input="onChangeInput"
+      />
+    </div>
+  </div>
+</template>
