6dd1d46c2e7c1da3a900c37dacf6cce421b4dc8e
jamols09 <jamols09@github.com>
2023-05-06 23:03:44 +0800

    feat: add logout feature

--- components/Navbar.vue
+++ components/Navbar.vue
@@ -1,3 +1,29 @@
+<script setup>
+const user = useSupabaseUser();
+const supabase = useSupabaseClient();
+
+const logout = async () => {
+  // 1) make user.value = null
+  // 2) remove jwt from cookies
+
+  //   const { error } = supabase.auth.signOut();
+  //   if (error) {
+  //     console.log(error);
+  //   }
+
+  try {
+    await $fetch("/api/_supabase/session", {
+      method: "POST",
+      body: { event: "SIGNED_OUT", session: null },
+    });
+  } catch (error) {
+    console.log(error);
+  }
+
+  user.value = null;
+  navigateTo("/");
+};
+</script>
 <template>
   <!-- NAVBAR -->
   <header
@@ -5,6 +31,11 @@
   >
     <!-- <a class="text-3xl font-mono" href="">cartrader</a> -->
     <NuxtLink class="text-3xl font-mono" to="/">Cartrader</NuxtLink>
+    <div v-if="user" class="flex">
+      <NuxtLink to="/profile/listings" class="mr-5">Profile</NuxtLink>
+      <p class="cursor-pointer">Logout</p>
+    </div>
+    <NuxtLink v-else to="/login">Login</NuxtLink>
   </header>
   <!-- NAVBAR -->
 </template>
--- pages/login.vue
+++ pages/login.vue
@@ -1,8 +1,28 @@
-<script setup></script>
+<script setup>
+definePageMeta({
+  layout: "custom",
+});
+const supabase = useSupabaseClient();
+
+const user = useSupabaseAuthClient(); //decode jwt
+
+const onLogin = async () => {
+  const { error } = supabase.auth.signInWithOAuth({
+    provider: "google",
+  });
+
+  if (error) {
+    console.log(error);
+  }
+};
+</script>
 <template>
   <div class="mt-10">
     <h1 class="text-5xl font-bold mb-7">Log in</h1>
-    <button class="bg-red-400 p-3 rounded text-white font-bold">
+    <button
+      @click="onLogin"
+      class="bg-red-400 p-3 rounded text-white font-bold"
+    >
       Login with Google
     </button>
   </div>
