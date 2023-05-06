2ef9db21545a1053cf2a0d13947bc86011a6d393
jamols09 <jamols09@github.com>
2023-05-06 19:07:57 +0800

    feat: create input components

---
+++ components/Car/Ad/Image.vue
@@ -0,0 +1,18 @@
+<template>
+  <div class="col-md-5 offset-md-1 mt-2 w-[100%]">
+    <label for="" class="text-cyan-500 mb-1 text-sm"> Image *</label>
+    <form class="mt-2">
+      <div class="form-group">
+        <div class="relative">
+          <input
+            type="file"
+            accept="image/*"
+            class="opacity-0 absolute cursor-pointer"
+          />
+          <span class="cursor-pointer"> Browse File </span>
+          
+        </div>
+      </div>
+    </form>
+  </div>
+</template>
---
+++ components/Car/Ad/Textarea.vue
@@ -0,0 +1,27 @@
+<script setup>
+const props = defineProps({
+  title: String,
+  name: String, //name of prop to be updated
+  placeholder: String,
+});
+
+const emit = defineEmits(["changeInput"]);
+const state = ref("");
+const onChange = () => {
+  emit("changeInput", state.value, props.name);
+};
+</script>
+
+<template>
+  <div class="flex flex-col w-[100%] mt-2">
+    <label for="" class="text-cyan-500 mb-1 text-sm"></label>
+    <textarea
+      type="text"
+      class="p-2 border w-100 rounded"
+      :placeholder="placeholder"
+      v-model="state"
+      :name="name"
+      @input="onChange"
+    />
+  </div>
+</template>
